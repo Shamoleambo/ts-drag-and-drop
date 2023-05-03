628ce0f5aa0909439d370c5fa50af526f905f1af
Tiago Gomes <tidgomes@gmail.com>
2023-05-03 16:42:07 -0300

    create ProjectState class and make sure it is a singleton

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,33 @@ function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjustedDescriptor
 }
 
+class ProjectState {
+  private projects: any[] = []
+  private static instance: ProjectState
+
+  private contructor() {}
+
+  static getInstance() {
+    if (this.instance) return this.instance
+
+    this.instance = new ProjectState()
+    return this.instance
+  }
+
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title,
+      description,
+      people: numOfPeople
+    }
+
+    this.projects.push(newProject)
+  }
+}
+
+const projectState = ProjectState.getInstance()
+
 class ProjectList {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
