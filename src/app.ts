c5935e7e104764ae4eaa23b9ed9bfb9445c63ccb
Tiago Gomes <tidgomes@gmail.com>
2023-05-04 14:20:45 -0300

    implement the methods from DragTarget interface

--- src/app.ts
+++ src/app.ts
@@ -198,7 +198,10 @@ class ProjectItem
   }
 }
 
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList
+  extends Component<HTMLDivElement, HTMLElement>
+  implements DragTarget
+{
   assignedProjects: Project[]
 
   constructor(private type: 'active' | 'finished') {
@@ -210,7 +213,26 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     this.renderContent()
   }
 
+  @autoBind
+  dragOverHandler(_: DragEvent): void {
+    const listEl = this.element.querySelector('ul')!
+    listEl.classList.add('droppable')
+  }
+
+  @autoBind
+  dropHandler(_: DragEvent): void {}
+
+  @autoBind
+  dragLeaveHandler(_: DragEvent): void {
+    const listEl = this.element.querySelector('ul')!
+    listEl.classList.remove('droppable')
+  }
+
   configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler)
+    this.element.addEventListener('drop', this.dropHandler)
+    this.element.addEventListener('dragleave', this.dragLeaveHandler)
+
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter((prj) => {
         if (this.type === 'active') return prj.status === ProjectStatus.Active
