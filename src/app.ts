68aff446f48112d68e6bd92a155a5eef3494ee3d
Tiago Gomes <tidgomes@gmail.com>
2023-05-04 08:44:26 -0300

    create a Project class to better type

--- src/app.ts
+++ src/app.ts
@@ -57,9 +57,24 @@ function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjustedDescriptor
 }
 
+enum ProjectStatus {
+  Active,
+  Finished
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
 class ProjectState {
   private listeners: any[] = []
-  private projects: any[] = []
+  private projects: Project[] = []
   private static instance: ProjectState
   private constructor() {}
 
@@ -75,12 +90,13 @@ class ProjectState {
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
+    const newProject = new Project(
+      Math.random().toString(),
       title,
       description,
-      people: numOfPeople
-    }
+      numOfPeople,
+      ProjectStatus.Active
+    )
 
     this.projects.push(newProject)
 
@@ -96,7 +112,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLElement
-  assignedProjects: any[]
+  assignedProjects: Project[]
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById(
